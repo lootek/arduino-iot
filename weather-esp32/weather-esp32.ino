5c8555c0ef3d09dabafefd5e005200b83b8fa4da
Piotr Lucinski <lootek87@gmail.com>
2024-09-23 18:50:58 +0200

    improved dht22 with mqtt

--- weather-esp32/weather-esp32.ino
+++ weather-esp32/weather-esp32.ino
@@ -1,27 +1,78 @@
-#include <DHT_U.h>
+#include <EspMQTTClient.h>
 #include <DHT.h>
+#include "/mnt/data/projects/arduino-playground/wifi-creds.h"
 
-#define DHT_SENSOR_PIN  4
-#define DHT_SENSOR_TYPE DHT22
+const bool debug = false;
 
-DHT dht_sensor(DHT_SENSOR_PIN, DHT_SENSOR_TYPE);
+EspMQTTClient client(
+  ssid_f,
+  password,
+  "192.168.10.18",      // MQTT Broker server ip
+  "",                   // MQTTUsername, Can be omitted if not needed
+  "",                   // MQTTPassword, Can be omitted if not needed
+  "arbor",              // Client name that uniquely identify your device
+  1883                  // The MQTT port, default to 1883. this line can be omitted
+);
 
-void setup() {
-  Serial.begin(9600);
-  dht_sensor.begin();
+DHT dht(4, DHT22);
+
+void setup_mqtt()
+{
+  client.enableDebuggingMessages(debug);
+  client.enableMQTTPersistence();
+  client.setKeepAlive(90);
+  client.enableLastWillMessage("/sensors/arbor/lastwill", "I am going offline");
 }
 
-void loop() {
-  float humidity  = dht_sensor.readHumidity();
-  float temperature_C = dht_sensor.readTemperature();
+void onConnectionEstablished()
+{
+  client.publish("/sensors/arbor/mqtt/status", "ready");
+  client.publish("/sensors/arbor/wifi/ip", String(client.getMqttServerIp()));
+}
 
-  if ( isnan(temperature_C) || isnan(humidity)) {
-    Serial.println("Failed to read from DHT sensor!");
+void setup()
+{
+  Serial.begin(115200);
+  dht.begin();
+  setup_mqtt();
+}
+
+void measure(Stream& serial) {
+  float t = dht.readTemperature();
+  if (isnan(t)) {
+    Serial.println("Temp measurement error");
   } else {
-    Serial.print(humidity);
-    Serial.print(temperature_C);
+    Serial.println(t);
+    client.publish("/sensors/arbor/dht22/temperature", String(t));
   }
 
-  // wait a 2 seconds between readings
-  delay(2000);
+  float h = dht.readHumidity();
+  if (isnan(h)) {
+    Serial.println("Humidity measurement error");
+  } else {
+    Serial.println(h);
+    client.publish("/sensors/arbor/dht22/humidity", String(h));
+  }
+  
+  if (debug) {
+    Serial.print("temperature = ");
+    Serial.println(t);
+
+    Serial.print("humidity = ");
+    Serial.println(h);
+  }
+}
+
+void loop()
+{
+  measure(Serial);
+
+  // MQTT
+  client.loop();
+
+  if (debug) {
+    delay(5 * 1000);
+  } else {
+    delay(30 * 1000);
+  }
 }
